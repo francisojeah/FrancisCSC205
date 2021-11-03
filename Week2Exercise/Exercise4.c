abbfa568ba66444b73b44b1d97d59b5f824e0cf5
francisojeah <ojeahfrancis@gmail.com>
2021-11-03 18:08:16 +0100

    Practical Exercises for week 2

---
+++ Week2Exercise/Exercise4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	char myInitial = 'F';
+	char myName[] = "Francis Okocha-Ojeah";
+	printf("Your name is = %s\n", myName);
+	printf("The initial of your firstname is = %c\n", myInitial);
+	return 0;
+
+}
+	
