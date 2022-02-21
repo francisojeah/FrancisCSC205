005319a9782eb697ab255e582a409558cdd30f9e
francisojeah <ojeahfrancis@gmail.com>
2022-02-21 15:03:54 +0100

    added exercises 6A,6B,6C on bitwise operators

---
+++ Week4/Exercises/Exercise6A.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	int first_int; second_int;
+	printf("Enter first integer: ");
+	scanf("%d", &first_int);
+	printf("Enter second integer: ");
+	scanf("%d", &second_int);
+	
+	printf("output = %d\n", first_int&second_int);
+	return 0;
+}
