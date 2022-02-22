6cde843af70f3dd85a44439025b24c0b3f8f1147
francisojeah <ojeahfrancis@gmail.com>
2022-02-22 08:14:05 +0100

    Process & THread Programming

---
+++ Week8/Exercises/Exercise6.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(){
+	if(fork() == 0)
+		if(fork())
+			printf("Job well done!!\n");
+	return 0;
+}
---
+++ Week8/Exercises/Exercise7.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void *childThread(void* args)
+{	
+	printf("The child process created this thread\n");
+	return NULL;
+}
+int main()
+{
+	int gap;
+	
+	printf("Before Process Created\n");
+	gap=fork();
+	printf("After Process Created\n");
+	
+	if (gap==0){
+		pthread_t pid;
+		printf("Child process before thread creation\n");
+		pthread_create(&pid, NULL, &childThread, NULL);
+		pthread_join(pid, NULL);
+		printf("Child Process after thread creation\n");
+	}
+	else{
+		printf("Parent process exist\n");
+	}
+	return 0;
+}
